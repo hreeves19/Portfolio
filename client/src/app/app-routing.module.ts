59a4ea00a1943bfdb3a9c2e33fcb81356009f5cd
Court Reeves <courtreeves6@gmail.com>
2021-01-06 16:44:41 -0500

    Adding products component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'products', component: ProductsComponent}
 ];
 
 @NgModule({
